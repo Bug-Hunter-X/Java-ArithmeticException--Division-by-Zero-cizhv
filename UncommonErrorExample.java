7091b86f0aa022cdb37de77815ec7a7bd9251201
BugHunter-X <askzenai@gmail.com>
2024-12-07 07:20:07 +0100

    Add UncommonErrorExample.java file

---
+++ UncommonErrorExample.java
@@ -0,0 +1,6 @@
+public class UncommonErrorExample {
+    public static void main(String[] args) {
+        int result = 10 / 0; // This line will throw an ArithmeticException
+        System.out.println("Result: " + result);
+    }
+}
